76d0adee3d00b97a394826237aaf9fb136c7fb5d
prasadt45 <146625931+prasadt45@users.noreply.github.com>
2025-01-22 17:56:13 +0530

    Updated Routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import {registerUser} from "../controllers/user.controller.js" 
+import {getUserChannelProfile, getWatchHistory, registerUser, updateUserAvatar, updateUserCoverImage} from "../controllers/user.controller.js" 
 import {upload} from "../middlewares/multer.middleware.js"
-import { loginUser  , refreshAccessToken} from "../controllers/user.controller.js";
+import { loginUser  , refreshAccessToken , changeCurrentPassword , getCurrentUser} from "../controllers/user.controller.js";
 import { logoutUser } from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-
+import { updateAccountDetails } from "../controllers/user.controller.js";
 
 const router = Router() 
 
@@ -27,5 +27,14 @@ router.route("/login").post(loginUser)
 // verifyJWT is middlware which will runs firsst after that logoutUser will be executed 
 router.route("/logout").post(verifyJWT , logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT , changeCurrentPassword)
+router.route("/current-user").get(verifyJWT , getCurrentUser)
+router.route("/update-details").patch(verifyJWT , updateAccountDetails)
+router.route("/avatar").patch(verifyJWT , upload.single("avatar")  , updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT , upload.single("coverimage") , updateUserCoverImage)
+// as we are using req.params in getUserChannelProfile 
+router.route("/c/:username").get(verifyJWT , getUserChannelProfile)
+router.route("/Watch-history").get(verifyJWT , getWatchHistory)
+
 export default router;
 
