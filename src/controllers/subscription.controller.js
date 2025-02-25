fbe450389fa58ecb464cdb70790c040274c8c928
prasadt45 <146625931+prasadt45@users.noreply.github.com>
2025-02-25 21:29:45 +0530

    Add subscriber and subscribed channels retrieval functionality

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -44,4 +44,106 @@ const toggleSubscription = asyncHandler(async(req , res)=>{
 
 })
 
-export {toggleSubscription} ;
+// controller to return subscriber list of a channel
+const getuserchannelsubscribers = asyncHandler(async(req , res)=>{
+    const {channelid} = req.params ;
+    if(!channelid){
+        throw new ApiError(400 , "Channel id is required")
+    }
+
+    const getsub = await Subscription.aggregate([
+        {
+            $match : {
+                channel : new mongoose.Types.ObjectId(channelid)
+            }
+        } , 
+        {
+            $lookup : {
+                from : "users" ,
+                localField : "subscriber",
+                foreignField : "_id",
+                as : "subscribers"
+                }
+        } , 
+        {
+            $unwind : "$subscribers"
+        }, 
+        {
+            $project:{
+                _id : 0 ,
+                subscriber : "$subscribers._id",
+                name : "$subscribers.name",
+                email : "$subscribers.email",
+                username : "$subscribers.username",
+                createdAt : 1
+         
+            }
+        }
+    ])
+
+    return res.status(200)
+    .json(
+        new Apiresponce(
+            200 ,
+            "Subscriber list of channel",
+            getsub
+        )
+
+    )
+
+
+})
+
+// controller to return channel list to which user has subscribed
+const getSubscribedChannels = asyncHandler(async (req, res) => {
+    const { subscriberId } = req.params
+
+    if (!subscriberId) {
+        throw new ApiError(400, "Subscriber id is required")
+    }
+
+    const getchannels  = await Subscription.aggregate([
+        {
+            $match: {
+                subscriber : new mongoose.Types.ObjectId(subscriberId)
+            
+            } 
+        },
+        { 
+            $lookup:{
+                from : "users",
+                localField : "channel",
+                foreignField : "_id",
+                as : "channel"
+            }
+
+        } , 
+        {
+            $unwind : "$channel"
+        } , 
+        {
+            $project:{
+                _id : 1 ,
+                name : "$channel.name",
+                email : "$channel.email",
+                username : "$channel.username",
+                createdAt : 1,
+                channel : "$channel._id",
+                subscriber : 1
+
+                }
+
+        }
+    ])
+
+    return res.status(200)
+    .json(
+        new Apiresponce(
+            200,
+            "Channels subscribed by user",
+            getchannels
+        )
+    )
+})
+
+export {toggleSubscription , getuserchannelsubscribers , getSubscribedChannels} ;
--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { toggleSubscription } from "../controllers/subscription.controller.js";
+import { toggleSubscription , getuserchannelsubscribers , getSubscribedChannels } from "../controllers/subscription.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router()  ;
 router.route("/toggle-subscription/:channelid").post( verifyJWT , toggleSubscription) ;
+router.route("/getuserchannelsubscribers/:channelid").get(verifyJWT , getuserchannelsubscribers) ;
+router.route("/getSubscribedChannels/:subscriberId").get(verifyJWT , getSubscribedChannels) ;
 
 export default router ;
